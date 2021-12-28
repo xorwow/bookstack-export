12bc143378329648177284b10a15974a876203b5
atlasax <32738599+atlasax@users.noreply.github.com>
2021-12-28 19:53:11 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Bookstack export config
+
+BOOKSTACK_URL = "https://your.bookstackinstance.com"
+TOKEN_ID = "" # auth token id of an administrator
+TOKEN_SECRET = "" # auth token secret of an administrator
+EXPORT_ROOT = "exports/" # abs. or rel. path to export parent directory
+EXPORT_TYPE = "pdf" # choose: pdf, html, plaintext
